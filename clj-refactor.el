e55ddd5df741394ac36e1ab75a3456141780ae9a
Wes Morgan <cap10morgan@gmail.com>
2015-12-27 15:55:20 -0500

    set version to 1.1.1-cvs

--- clj-refactor.el
+++ clj-refactor.el
@@ -6,7 +6,7 @@
 ;; Author: Magnar Sveen <magnars@gmail.com>
 ;;         Lars Andersen <expez@expez.com>
 ;;         Benedek Fazekas
-;; Version: 1.1.0
+;; Version: 1.1.1-cvs
 ;; Keywords: convenience, clojure, cider
 ;; Package-Requires: ((emacs "24.3") (s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (cider "0.9.1") (edn "1.1.1"))
 
